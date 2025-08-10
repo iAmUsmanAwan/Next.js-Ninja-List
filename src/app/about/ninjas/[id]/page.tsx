7ef2a7ab19fe783be05a8b30065571326578fad7
Admiral General <muhammadusmanawan88@gmail.com>
2025-08-11 04:29:45 +0500

    Initial commit

--- src/app/about/ninjas/[id]/page.tsx
+++ src/app/about/ninjas/[id]/page.tsx
@@ -6,13 +6,37 @@ export const metadata: Metadata = {
     description: 'Details about our ninjas and activities.',
 };
 
-export default function DetailsPage() {
+export async function generateStaticParams() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const ninjas = await res.json();
+
+    return ninjas.map((ninja: any) => ({
+        id: ninja.id.toString(),
+    }));
+}
+
+async function getNinja(id: string) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    return res.json();
+}
+
+export default async function DetailsPage({ params }: { params: { id: string } }) 
+{
+    const ninja = await getNinja(params.id);
     return (
         <>
         <h1>Details Page</h1>
+
+        <h1>{ ninja.name }</h1>
+        <p> { ninja.email }</p>
+        <p> { ninja.website }</p>
+        <p> { ninja.address.city }</p>
+
+        <br /><br /><br /><br />
         
-        <Link href="/">Go to Home Page</Link> <br /><br /><br />
-        <Link href="/about/ninjas">Go to Ninjas Page</Link>
+        <Link href="/about/ninjas">Go to Ninjas Page</Link><br /><br />
+        <Link href="/">Go to Home Page</Link> 
+
         </>
     );
 }
