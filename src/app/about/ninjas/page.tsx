762ab5310d36cbda37cac11adc79cd78d6a2a915
Admiral General <muhammadusmanawan88@gmail.com>
2025-08-10 13:03:14 +0500

    8

--- src/app/about/ninjas/page.tsx
+++ src/app/about/ninjas/page.tsx
@@ -1,18 +1,40 @@
 import Link from 'next/link';
 import type { Metadata } from 'next';
+import styles from './Ninjas.module.css';
 
-// This sets the title for ONLY the Ninjas page
+// Metadata for this page only
 export const metadata: Metadata = {
-  title: 'Ninja List | Ninja page',
-  description: 'Learn more about the Ninja List team.',
+    title: 'Ninja List | Ninja page',
+    description: 'Learn more about the Ninja List team.',
 };
 
-// This is the ninja page of the application.
+// Fetch data (Server Component)
+async function getNinjas() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    return res.json();
+}
 
-export default function NinjaPage() {
-    return <> 
-    <h1>Ninja page (About Us)</h1><br /><br /><br />
-    <h3>All Ninjas wear black for stealth</h3><br /><br /><br />
-    <Link href="/">Go to Home Page</Link>
-    </>;
+export default async function NinjaPage() {
+    const ninjas = await getNinjas();
+
+    return (
+        <>
+        <h1>Ninja page (About Us)</h1>
+        <h3>All Ninjas wear black for stealth</h3>
+        <br /><br /><br />
+
+        {ninjas.map((ninja: any) => (
+            <Link
+            href={`/ninjas/${ninja.id}`}
+            key={ninja.id}
+            className={styles.single}
+            >
+            <h3>{ninja.name}</h3>
+            </Link>
+        ))}
+
+        <br /><br /><br />
+        <Link href="/">Go to Home Page</Link>
+        </>
+    );
 }
