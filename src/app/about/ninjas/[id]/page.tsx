3685699d02b53abc5ec5272403f6e17c586af802
Admiral General <muhammadusmanawan88@gmail.com>
2025-08-11 06:50:15 +0500

    11

--- src/app/about/ninjas/[id]/page.tsx
+++ src/app/about/ninjas/[id]/page.tsx
@@ -1,13 +1,12 @@
-import Link from 'next/link';
-import type { Metadata } from 'next';
+import Link from "next/link";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Ninja List | Details Page',
-    description: 'Details about our ninjas and activities.',
+    title: "Ninja List | Details Page",
+    description: "Details about our ninjas and activities.",
 };
 
-// Define Ninja type
-interface Ninja {
+type Ninja = {
     id: number;
     name: string;
     email: string;
@@ -15,10 +14,10 @@ interface Ninja {
     address: {
         city: string;
     };
-}
+};
 
 export async function generateStaticParams() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const ninjas: Ninja[] = await res.json();
 
     return ninjas.map((ninja) => ({
@@ -27,26 +26,38 @@ export async function generateStaticParams() {
 }
 
 async function getNinja(id: string): Promise<Ninja> {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+    );
     return res.json();
 }
 
-export default async function DetailsPage({ params }: { params: { id: string } }) {
-    const ninja = await getNinja(params.id);
+export default async function DetailsPage({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+
+    const { id } = await params;
+    const ninja = await getNinja(id);
 
     return (
         <>
-            <h1>Details Page</h1>
+        <h1>Details Page</h1>
+        <h1>{ninja.name}</h1>
+        <p>{ninja.email}</p>
+        <p>{ninja.website}</p>
+        <p>{ninja.address.city}</p>
 
-            <h1>{ninja.name}</h1>
-            <p>{ninja.email}</p>
-            <p>{ninja.website}</p>
-            <p>{ninja.address.city}</p>
+        <br />
+        <br />
+        <br />
+        <br />
 
-            <br /><br /><br /><br />
-            
-            <Link href="/about/ninjas">Go to Ninjas Page</Link><br /><br />
-            <Link href="/">Go to Home Page</Link>
+        <Link href="/about/ninjas">Go to Ninjas Page</Link>
+        <br />
+        <br />
+        <Link href="/">Go to Home Page</Link>
         </>
     );
 }
